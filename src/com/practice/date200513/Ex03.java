b4fb4a434ed2cdfc66e64ecdac93a86eb2959ae2
'Yaezy-Hwang' <'yeahz3564@gmail.com'>
2020-05-13 17:38:27 +0900

    pratice self : Array

---
+++ src/com/practice/date200513/Ex03.java
@@ -0,0 +1,24 @@
+package com.practice.date200513;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		
+		//배열에서 유효한 값만 출력하기
+		
+		double[] data = new double[5];
+		
+		int size = 0;
+		
+		data[0] = 10.0; size++;
+		data[1] = 20.0; size++;
+		data[2] = 30.0; size++;	
+		// 1. 배열의 비어있는 값은 size++이 없음.
+		
+		for(int i=0; i<size; i++) { //2. 1때문에 비어있는 값은 i=size가 되어 반복문 탈출
+			System.out.println(data[i]);
+		}
+		
+
+	}
+}
